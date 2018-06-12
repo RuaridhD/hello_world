0dfc3eb72e34a60f47607873a781837262aaad88
Ruaridh <ruaridhdunbar@gmail.com>
2018-06-12 08:01:53 +0100

    adds 'hello world' program file

---
+++ program.py
@@ -0,0 +1,7 @@
+print('---------------------')
+print('    HELLO APP')
+print('---------------------')
+user_text = input("Hey, what's your name? ")
+
+greeting = ('Nice to meet you ' + user_text)
+print(greeting)
